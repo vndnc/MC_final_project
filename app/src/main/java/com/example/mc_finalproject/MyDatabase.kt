c334d0f5c7821a75ef6ac22227c5443e2c9df3b5
Moon Seongyu <kakao0228@mju.ac.kr>
2023-06-07 14:40:05 +0900

    db 기본 코드 강의안 기반

---
+++ app/src/main/java/com/example/mc_finalproject/MyDatabase.kt
@@ -0,0 +1,67 @@
+package com.example.mc_finalproject
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
+import android.util.Log
+
+class MyDatabase {
+    object MyDBContract {
+        object MyEntry: BaseColumns {
+            const val TABLE_NAME = "myDBfile"
+            const val image = "image"
+            const val date = "date"
+            const val name = "name"
+            const val comment = "comment"
+        }
+    }
+
+    class MyDbHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+        val SQL_CREATE_ENTRIES =
+            "CREATE TABLE ${MyDBContract.MyEntry.TABLE_NAME} (" +
+                    "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+                    "${MyDBContract.MyEntry.image} BLOB," +
+                    "${MyDBContract.MyEntry.date} TEXT," +
+                    "${MyDBContract.MyEntry.name} TEXT," +
+                    "${MyDBContract.MyEntry.comment} TEXT"
+        val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${MyDBContract.MyEntry.TABLE_NAME}"
+
+        override fun onCreate(db: SQLiteDatabase) {
+            db.execSQL(SQL_CREATE_ENTRIES)
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+            db.execSQL(SQL_DELETE_ENTRIES)
+            onCreate(db)
+        }
+
+        override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+            onUpgrade(db, oldVersion, newVersion)
+        }
+        companion object {
+            const val DATABASE_VERSION = 1
+            const val DATABASE_NAME = "myDBfile.db"
+        }
+        fun selectAll(): MutableList<MyElement> {
+            val readList = mutableListOf<MyElement>()
+            val db = readableDatabase
+            val cursor = db.rawQuery("SELECT * FROM " + MyDBContract.MyEntry.TABLE_NAME + ";", null)
+            Log.d("TAG", "Select All Query: " + "SELECT * FROM " + MyDBContract.MyEntry.TABLE_NAME + ";")
+            Log.d("TAG", cursor.toString())
+            with(cursor){
+                while (moveToNext()){
+                    readList.add(MyElement(
+                        cursor.getBlob(1),
+                        cursor.getString(2),
+                        cursor.getString(3),
+                        cursor.getString(4))
+                    )
+                }
+            }
+            cursor.close()
+            db.close()
+            return readList
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mc_finalproject/MyElement.kt
@@ -0,0 +1,6 @@
+package com.example.mc_finalproject
+
+import android.graphics.Bitmap
+import java.sql.Blob
+
+class MyElement(val image: ByteArray, val date: String, val name: String, val commend: String)
